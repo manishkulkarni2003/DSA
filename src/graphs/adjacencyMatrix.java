7aaaa183282f29863a5cd418f0a992871d42dff7
manishkulkarni2003 <mk3304939@gmail.com>
2025-05-12 21:47:15 +0530

    Graphs

---
+++ src/graphs/adjacencyMatrix.java
@@ -0,0 +1,16 @@
+package graphs;
+
+public class adjacencyMatrix {
+	public static void main(String[] args) {
+	int n=3;
+	int m=3;
+	int [][]adj=new int[n+1][n+1];
+	
+	//edge 1--2
+	adj[1][2]=1;
+	adj[2][1]=1;
+	
+	
+	
+	}
+}
